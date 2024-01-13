84f13f5b8aebad6a8ba505bb33c71695a37ce119
Ishan Sharma <ishu.ishan3004@gmail.com>
2024-01-12 17:04:19 -0800

    Commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -7,7 +7,7 @@ export default function About(){
                 <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex-col md:items-start md:text-left mb-16 md:mb-0 items-enter text-center">
                     <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
                         Hi, I'm Ishan.
-                        <br className="hidden lg:inline-block"/>I love to Build amazing apps.
+                        <br className="hidden lg:inline-block"/>I love to Solve Problems.
                     </h1>
                     <p className="mb-8 leading-relaxed">
                     Dedicated and efficient Research Engineer offering to support organizational goals with targeted research design and specialized parameters according to leadership. Collaborative team member with extensive experience supporting research design and amending variables to target shifting objectives. I am a systematic statistical analyst proficient in Python programming and problem-solving for multi-dimensional results analysis and hypothesis projection.
